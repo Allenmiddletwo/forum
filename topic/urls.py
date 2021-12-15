cda514476caf663346e40f98233e262fc24bd13b
Allenmiddletwo <10930305@dcsh.tp.edu.tw>
2021-12-15 12:08:08 +0800

    討論主題

--- topic/urls.py
+++ topic/urls.py
@@ -1,26 +1,9 @@
-from django.views.generic import *
-from django.urls import reverse
-from .models import *
+from django.urls import path
+from .views import *
 
-# 討論主題列表
-class TopicList(ListView):
-    model = Topic
-    ordering = ['-created']
-    paginate_by = 20        # 每頁主題數
-
-# 新增討論主題
-class TopicNew(CreateView):
-    model = Topic
-    fields = ['subject', 'content']
-
-    def get_success_url(self):
-        return reverse('topic_list')
-
-    def form_valid(self, form):
-        # 自動將目前使用者填入討論主題的作者欄
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-# 檢視討論主題
-class TopicView(DetailView):
-    model = Topic
+urlpatterns = [
+    path('', TopicList.as_view(), name='topic_list'),
+    path('new/', TopicNew.as_view(), name='topic_new'), 
+    path('<int:pk>/', TopicView.as_view(), name='topic_view'),
+    path('<int:tid>/reply/', TopicReply.as_view(), name='topic_reply'),
+]
